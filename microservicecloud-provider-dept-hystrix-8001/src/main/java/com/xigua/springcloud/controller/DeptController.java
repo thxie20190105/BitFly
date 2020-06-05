b042aab88a7f63cb1e5a75611d0cdb668afc9c6b
xigua <thxie2019@gmail.com>
2020-06-05 13:33:24 +0800

    0605-HystrixCommand

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/xigua/springcloud/controller/DeptController.java
@@ -0,0 +1,90 @@
+package com.xigua.springcloud.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.xigua.springcloud.entities.Dept;
+import com.xigua.springcloud.server.DeptServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author xigua
+ * @description
+ * @date 2020/6/4
+ **/
+@RestController
+public class DeptController {
+
+    private DeptServer server;
+
+
+    /**
+     * 发现客户端，对外提供自己的服务情况
+     */
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    public DeptController(DeptServer server, DiscoveryClient discoveryClient) {
+        this.server = server;
+        this.discoveryClient = discoveryClient;
+    }
+
+    @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+    public boolean add(Dept dept) {
+        return server.add(dept);
+    }
+
+
+    @RequestMapping(value = "/dept/get", method = RequestMethod.GET)
+    //一旦调用服务方法失败并且抛出异常，会自动调用下面的fallbackMethod方法
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
+    public Dept get(@PathVariable("id") Long id) {
+        Dept dept = server.get(id);
+        if (dept == null) {
+            throw new RuntimeException("为找到ID");
+
+        }
+        return dept;
+    }
+
+    public Dept processHystrix_Get(@PathVariable("id") long id) {
+        return new Dept().setDeptno(id).setDname("该ID" + id + "没有对应有效信息").setDb_source("no this database in mysql");
+    }
+
+
+    @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+    public List<Dept> list() {
+        return server.list();
+    }
+
+    /**
+     * @return
+     */
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+
+        //盘点eureka里一共有多少微服务
+        List<String> list = discoveryClient.getServices();
+        System.out.println("共有服务" + list.size() + "个");
+
+
+        //指定具体微服务的信息
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("microservicecloud-dept");
+
+        for (ServiceInstance serviceInstance : serviceInstances) {
+            System.out.println(serviceInstance.getServiceId());
+            System.out.println(serviceInstance.getUri());
+            System.out.println(serviceInstance.getHost());
+            System.out.println(serviceInstance.getPort());
+
+        }
+        return this.discoveryClient;
+    }
+
+}
---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/xigua/springcloud/server/DeptServer.java
@@ -0,0 +1,19 @@
+package com.xigua.springcloud.server;
+
+import com.xigua.springcloud.entities.Dept;
+
+import java.util.List;
+
+/**
+ * @author xigua
+ * @description
+ * @date 2020/6/4
+ **/
+public interface DeptServer {
+    boolean add(Dept dept);
+
+    Dept get(Long id);
+
+    List<Dept> list();
+
+}
