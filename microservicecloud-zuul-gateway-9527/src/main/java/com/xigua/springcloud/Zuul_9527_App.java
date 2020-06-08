308ca0a12acce11797a27d38a83e3ccc0c5dd08c
xigua <thxie2019@gmail.com>
2020-06-08 14:07:39 +0800

    0608-zuul

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/xigua/springcloud/Zuul_9527_App.java
@@ -0,0 +1,18 @@
+package com.xigua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author xigua
+ * @description EnableZuulProxy起到一个代理的作用
+ * @date 2020/6/8
+ **/
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_App {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_App.class, args);
+    }
+}
