b02ab3fe7ceac9f60b37cf130c2cf6bca69dda59
xigua <thxie2019@gmail.com>
2020-06-10 21:52:14 +0800

    0610-mq+spring集成mq

--- microservicecloud-spring-mq/src/main/java/com/xigua/activemq/spring/MqCon.java
+++ microservicecloud-spring-mq/src/main/java/com/xigua/activemq/spring/MqCon.java
@@ -1,9 +1,29 @@
 package com.xigua.activemq.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
 /**
  * @author xigua
  * @description
  * @date 2020/6/10
  **/
+@Service
 public class MqCon {
+    private JmsTemplate template;
+
+    @Autowired
+    public MqCon(JmsTemplate template) {
+        this.template = template;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MqCon mqCon = (MqCon) applicationContext.getBean("mqCon");
+        String o = (String) mqCon.template.receiveAndConvert();
+        System.out.println(o);
+    }
 }
--- microservicecloud-spring-mq/src/main/java/com/xigua/activemq/spring/MqPro.java
+++ microservicecloud-spring-mq/src/main/java/com/xigua/activemq/spring/MqPro.java
@@ -1,9 +1,35 @@
 package com.xigua.activemq.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.jms.TextMessage;
+
 /**
  * @author xigua
  * @description
  * @date 2020/6/10
  **/
+@Service
 public class MqPro {
+
+    private JmsTemplate template;
+
+    @Autowired
+    public MqPro(JmsTemplate template) {
+        this.template = template;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //默认首字母小写
+        MqPro mqPro = (MqPro) applicationContext.getBean("mqPro");
+        mqPro.template.send(session -> {
+            TextMessage textMessage = session.createTextMessage("spring+mq整合练习");
+            return textMessage;
+        });
+    }
 }
